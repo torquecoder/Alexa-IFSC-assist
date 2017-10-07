f51f4b0e56fb8e19b6cede42d3f7715f3054a952
Shubham Singhal <torquecode@gmail.com>
2017-10-08 01:14:32 +0530

    Add launch conversation

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import requests
+import logging
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+@ask.launch
+def new_request():
+    return question("What do you want me to do?")
+
+@ask.intent("FetchIFSCdetails")
+def fetch_IFSC_details():
+    return question('What is IFSC code?').reprompt('May I please have the IFSC code?')
+
+port = int(os.getenv('PORT', 5000))
+app.run(debug=False, port=port, host='0.0.0.0')
